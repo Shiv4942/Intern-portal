c3d8fdf7f676d2f49932eb8782ce954a742ed13e
Shiv4942 <162607009+Shiv4942@users.noreply.github.com>
2025-08-02 15:03:23 +0530

    ds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-// import { Inter } from 'next/font/google';import './globals.css';
+// import { Inter } from 'next/font/google';
+import './globals.css';
 
 // const inter = Inter({ subsets: ['latin'] });
 import Navbar from './components/Navbar';
